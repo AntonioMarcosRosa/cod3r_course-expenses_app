3f6555b0c456ba6566a6b9d8e4368d51505054d7
Antônio Marcos <antoniomarcos.s.rosa@gmail.com>
2022-09-24 01:51:23 -0300

    feat - new dependecy added to the project (intl)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -49,7 +50,46 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: _transaction
                 .map((transaction) => Card(
-                      child: Text(transaction.title.toString()),
+                      child: Row(children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 8,
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          )),
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            'R\$ ${transaction.value?.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              transaction.title.toString(),
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              DateFormat('d MMM y').format(transaction.date),
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        ),
+                      ]),
                     ))
                 .toList(),
           )
--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -2,7 +2,7 @@ class Transaction {
   final String? id;
   final String? title;
   final double? value;
-  final DateTime? date;
+  final DateTime date;
 
   Transaction({
     required this.id,
